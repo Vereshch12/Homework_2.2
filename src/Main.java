ba3363365289760ab9cccbefe9b5fa10b6b5f61e
Vereshchagin_Mikhail <farasers@mail.ru>
2022-10-05 21:24:53 +0300

    Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Person maxim = new Person("Максим", 1987, "Минск");
+        Person maxim = new Person("Максим", 1987, "Минск", "бренд-менеджер");
         maxim.welcome();
-        Person anya = new Person("Аня", 1993, "Москва");
+        Person anya = new Person("Аня", 1993, "Москва", "методист образовательных программ\n");
         anya.welcome();
-        Person katya = new Person("Катя", 1994, "Калининград");
+        Person katya = new Person("Катя", 1994, "Калининград", "продакт-менеджер");
         katya.welcome();
-        Person artem = new Person("Артём", 1995, "Москва");
+        Person artem = new Person("Артём", 1995, "Москва", "директор по развитию бизнеса");
         artem.welcome();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,14 +2,16 @@ public class Person {
     private String name;
     private Integer yearOfBirth;
     private String city;
+    private String job;
 
-    public Person(String name, Integer yearOfBirth, String city) {
+    public Person(String name, Integer yearOfBirth, String city, String job) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
         this.city = city;
+        this.job = job;
     }
 
     public void welcome(){
-        System.out.printf("Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Будем знакомы!\n", name, city, yearOfBirth);
+        System.out.printf("Привет! Меня зовут %s. Я из города %s. Я родился в %d году. Я работаю на должности %s. Будем знакомы!\n", name, city, yearOfBirth, job);
     }
 }
