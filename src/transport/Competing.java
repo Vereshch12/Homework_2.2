15ef291d08e60259ca79fffcf4042ea1d79be47c
Vereshchagin_Mikhail <farasers@mail.ru>
2022-11-11 23:53:44 +0300

    Полиморфизм. ДЗ №2.

---
+++ src/transport/Competing.java
@@ -0,0 +1,14 @@
+package transport;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface Competing {
+
+    void pitStop();
+
+    Integer bestTime();
+
+    Integer maxSpeed();
+
+
+}
